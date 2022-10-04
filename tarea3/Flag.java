e3def2594ded9591403569261158339b75703bcd
RuthVazquez4 <ruth_vazquez4@hotmail.com>
2022-10-04 18:33:05 -0500

    se agrego range.java

---
+++ tarea3/Flag.java
@@ -0,0 +1,15 @@
+/*
+* Flag.java
+* Author: Ruth Vazquez Ruiz
+* Date: Oct 4, 2022
+* Location Ixtac, Ver. Mexico
+*/
+
+class Flag {
+  public static void main(String[] args) {
+
+    boolean flag = false;           
+    System.out.println("Valor de flag: ");        // define a boolean var
+    System.out.println(!!flag);     // prints true 
+  }
+}
