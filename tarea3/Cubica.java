0ff9b163f926a59320f0d0a2b4cc16a6cca5be0d
RuthVazquez4 <ruth_vazquez4@hotmail.com>
2022-10-28 10:49:28 -0500

    Add, scripts java

---
+++ tarea3/Cubica.java
@@ -0,0 +1,13 @@
+
+public class Cubica {
+    public static void main(String args[]){
+
+        double n = 10;
+
+        for (double i = -10; i <= n; i=i+1){
+            System.out.println(Math.pow(i, 3));
+   }
+
+
+}
+}
